72acbed8ccb0c82a9c171e9386f6477b7da6e32a
muaad <muadh24@gmail.com>
2014-03-31 11:10:14 +0300

    Added mandrill details

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,7 +79,7 @@ Jointregistrydatabase::Application.configure do
       :password  => ENV["MANDRILL_APIKEY"]
     }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => '41.242.2.160' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
