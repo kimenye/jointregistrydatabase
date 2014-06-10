adf6653145b6558d32bc457ab8c749e18e946873
Trevor Kimenye <kimenye@gmail.com>
2014-06-10 20:35:30 +0300

    added annotations for the various new models

--- app/models/inter_operative_complication.rb
+++ app/models/inter_operative_complication.rb
@@ -1,2 +1,13 @@
+# == Schema Information
+#
+# Table name: inter_operative_complications
+#
+#  id                :integer          not null, primary key
+#  name              :string(255)
+#  complication_type :string(255)
+#  created_at        :datetime
+#  updated_at        :datetime
+#
+
 class InterOperativeComplication < ActiveRecord::Base
 end
--- app/models/surgery_complication.rb
+++ app/models/surgery_complication.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: surgery_complications
+#
+#  id                              :integer          not null, primary key
+#  inter_operative_complication_id :integer
+#  surgery_id                      :integer
+#  created_at                      :datetime
+#  updated_at                      :datetime
+#
+
 class SurgeryComplication < ActiveRecord::Base
   belongs_to :inter_operative_complication
   belongs_to :surgery
